cde45c615e1f84094383b6e6792ab51e728dc3d4
activeguild <workdrive0306@gmail.com>
2021-10-10 00:56:54 +0900

    Update types

--- src/index.ts
+++ src/index.ts
@@ -1,9 +1,9 @@
-import { default as cn, Argument } from "classnames";
+import { default as cn, Argument, Value } from "classnames";
 
 type StringLiteral<T> = T extends `${string & T}` ? T : never;
 
-export const classnames = <T extends Argument>(
-  ...args: (T | Partial<Record<StringLiteral<T>, boolean>>)[]
+export const classNames = <T extends Argument>(
+  ...args: (Value | T | Partial<Record<StringLiteral<T>, boolean>>)[]
 ) => cn(args);
 
 export const classNamesFunc =
