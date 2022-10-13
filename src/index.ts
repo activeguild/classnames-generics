403c65c506cbc71e4be7a7cc132c70d76265f77d
j1ngzoue <workdrive0306@gmail.com>
2022-10-13 10:41:46 +0900

    chore: follow changes in the classnames library

--- src/index.ts
+++ src/index.ts
@@ -2,12 +2,12 @@ import { default as cn} from "classnames";
 
 type StringLiteral<T> = T extends `${string & T}` ? T : never;
 
-export const classNames = <T>(
+export const classNames = <T extends string>(
   ...args: (T | Partial<Record<StringLiteral<T>, boolean>>)[]
 ) => cn(args);
 
 export const classNamesFunc =
-  <T>() =>
+  <T extends string>() =>
   (...args: (T | Partial<Record<StringLiteral<T>, boolean>>)[]) => {
     return cn(args);
   };
